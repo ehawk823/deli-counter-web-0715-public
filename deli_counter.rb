8e6b0fcdb0dc9b1ee41ec1395023fd78f2872e6d
ehawk823 <evan.alexander.hawk@gmail.com>
2015-08-02 19:28:24 -0500

    done

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,29 @@
-# Write your code here.
+def line(array)
+  if array[0] == nil
+    puts "The line is currently empty."
+  else
+    puts "The line is currently: 1. #{array[0]} 2. #{array[1]} 3. #{array[2]}"
+  end
+end
+
+def take_a_number(array, person)
+  array << person
+  puts "Welcome, #{person}. You are number #{array.index(person) + 1} in line."
+end
+
+def now_serving(array)
+  if array[0] == nil
+    puts "There is nobody waiting to be served!"
+  else
+    puts "Currently serving #{array[0]}."
+    array.shift
+  end
+end
+
+
+
+
+
+
+
+
